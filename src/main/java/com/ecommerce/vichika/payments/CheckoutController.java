af1efeb776b95cb6865401139e43d1a22a845e30
vicheka <vichekafc07@gmail.com>
2026-02-19 21:00:42 +0700

    E-commerce

--- src/main/java/com/ecommerce/vichika/payments/CheckoutController.java
+++ src/main/java/com/ecommerce/vichika/payments/CheckoutController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/checkout")
+@RequestMapping("/api/v1/checkout")
 @RequiredArgsConstructor
 public class CheckoutController {
 
